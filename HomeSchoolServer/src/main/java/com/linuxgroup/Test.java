a09b22463a9af589a123f2c2a39483ab3b2bf2bd
tanhehe <443016215@qq.com>
2014-09-09 19:47:48 +0800

    配置好 SSH

---
+++ HomeSchoolServer/src/main/java/com/linuxgroup/Test.java
@@ -0,0 +1,16 @@
+package com.linuxgroup;
+
+import com.opensymphony.xwork2.Action;
+
+/**
+ * Created by tan on 14-9-9.
+ */
+public class Test implements Action {
+    @Override
+    public String execute() throws Exception {
+
+
+
+        return Action.SUCCESS;
+    }
+}
