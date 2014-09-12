49eaf92c6670fb04438fedf38483e5da3593fca5
tanhehe <443016215@qq.com>
2014-09-12 11:09:50 +0800

    构建 PushService，使用 spring 管理 PushService

--- HomeSchoolServer/src/main/java/com/linuxgroup/service/impl/PushServiceImpl.java
+++ HomeSchoolServer/src/main/java/com/linuxgroup/service/impl/PushServiceImpl.java
@@ -13,13 +13,39 @@ import cn.jpush.api.push.model.audience.AudienceTarget;
 import cn.jpush.api.push.model.notification.IosNotification;
 import cn.jpush.api.push.model.notification.Notification;
 import cn.jpush.api.report.ReceivedsResult;
+import com.linuxgroup.factory.PushPayLoadFactory;
 import com.linuxgroup.service.PushService;
+import org.springframework.messaging.handler.annotation.Payload;
 
 /**
  * Created by tan on 14-9-11.
  */
 public class PushServiceImpl implements PushService {
 
+    private String masterSecret;
+    private String appKey;
+    private Integer maxRetryTimes;
+
+    private JPushClient jPushClient;
+
+    public PushServiceImpl(String masterSecret, String appKey, Integer maxRetryTimes) {
+        this.masterSecret = masterSecret;
+        this.appKey = appKey;
+        this.maxRetryTimes = maxRetryTimes;
+
+        jPushClient = new JPushClient(masterSecret, appKey, maxRetryTimes);
+
+    }
+
+    public PushResult pushToAll(String message) throws APIConnectionException, APIRequestException {
+        PushPayload payload = PushPayLoadFactory.buildAllAllAlert(message);
+
+        PushResult result = jPushClient.sendPush(payload);
+
+        return result;
+    }
+
+    /*
     public static void main(String[] args) {
         JPushClient jPushClient = new JPushClient("2a090943a34259bca2745744", "8a306f5ee8eb3f339a8a8ad0", 3);
 
@@ -35,11 +61,7 @@ public class PushServiceImpl implements PushService {
         } catch (APIRequestException e) {
             e.printStackTrace();
         }
-
-    }
-
-
-
-
-
+    }*/
 }
+
+
