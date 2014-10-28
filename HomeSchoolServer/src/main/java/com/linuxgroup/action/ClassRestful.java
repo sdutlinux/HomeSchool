e9456e98f8b002c7e789ad047884ebd44a3bdb2b
tanhehe <443016215@qq.com>
2014-10-28 11:46:54 +0800

    update readme

---
+++ HomeSchoolServer/src/main/java/com/linuxgroup/action/ClassRestful.java
@@ -0,0 +1,13 @@
+package com.linuxgroup.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by tan on 14-10-28.
+ */
+@Controller
+@RequestMapping("/class")
+public class ClassRestful {
+
+}
