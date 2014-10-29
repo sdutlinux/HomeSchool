9ac0b3d4e9e160ec6e3c4637924f803e60ec845d
tanhehe <443016215@qq.com>
2014-10-29 09:58:32 +0800

    创建Class界面

--- HomeSchoolClient/app/src/main/java/com/linuxgroup/homeschool/client/ui/fragment/ClassFragment.java
+++ HomeSchoolClient/app/src/main/java/com/linuxgroup/homeschool/client/ui/fragment/ClassFragment.java
@@ -1,5 +1,6 @@
 package com.linuxgroup.homeschool.client.ui.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import com.linuxgroup.homeschool.client.R;
+import com.linuxgroup.homeschool.client.ui.CreateClassActivity;
 import com.linuxgroup.homeschool.client.utils.ToastUtils;
 
 import butterknife.ButterKnife;
@@ -53,7 +55,10 @@ public class ClassFragment extends Fragment {
         bt_create_class.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ToastUtils.showShort("创建班级");
+                Intent intent = new Intent(getActivity(), CreateClassActivity.class);
+                startActivity(intent);
+
+                // 创建完之后,等着接收创建成功的 broadcast, 然后从数据库读取
             }
         });
 
